3a6013eb86af04b194fe3f6e8d91d88ddef5b294
Secret6230 <akinkale054@hotmail.com>
2024-06-25 17:50:31 +0200

    projekt-3-tage

---
+++ projekt.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', function() {
+    window.location.href = 'Login.jsx';
+});
+
+
+function nextPage() {
+    window.location.href = 'Zweite_Seite.html';
+}
+
+
+
+function previousPage() {
+    window.location.href = 'Erste_Seite.html';
+}
